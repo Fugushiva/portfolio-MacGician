b839251632be920cd623c16795d36fcfbe2e2939
=Fugushiva <2111jedelodder@student.epfc.eu>
2023-09-21 15:33:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Jérôme Delodder">
+    <meta name="description" content="Portfolio Constantin MacGician Dimitriadis">
+    <link rel="stylesheet" href="assets/style/style.css">
+    <link rel="shortcut icon" href="images/logo.svg" type="image/x-icon">
+
+    <title>porfolio MacGician</title>
+</head>
+<body>
+    <header>
+        <div id="const">MacGician : Music Composer, arranger from Belgium with MAO</div>
+        <div id="logo"><a href="https://soundcloud.com/macgician" title="mon SoundCloud" target="_blank"><img src="images/logo.svg" alt="Logo de MacGician"></a></div>
+        <div id="navBox">
+            <nav>
+                <ul>
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">about</a></li>
+                    <li><a href="#">porfolio</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <div id ="whiSq1"></div>
+</body>
+</html>
